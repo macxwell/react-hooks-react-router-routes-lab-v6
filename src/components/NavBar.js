8f1e0ff7a52a33a7249b6c33399d1a6b314201cf
macxwell <omondimacxwell@gmail.com>
2024-08-17 14:50:02 +0300

    addition of navbar component

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -4,7 +4,15 @@ import "./NavBar.css";
 function NavBar() {
   return (
     <nav className="navbar">
-    
+      <NavLink to="/" key="home">
+        Home
+      </NavLink>
+      <NavLink to="/directors" key="directors">
+        Directors
+        </NavLink> 
+        <NavLink to="/actors" key="actors">
+          Actors
+          </NavLink>   
     </nav>
     );
 };
